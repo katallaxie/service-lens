8186981a21ed937205c276b52ad7b422ca8bc7d6
Sebastian Döll <sebastian@katallaxie.dev>
2025-10-05 20:53:09 +0000

    fix: drawer display

--- internal/components/layout.go
+++ internal/components/layout.go
@@ -36,106 +36,97 @@ func Wrap(p WrapProps, children ...htmx.Node) htmx.Node {
 
 // Layout is a whole document to output.
 func Layout(p LayoutProps, children ...htmx.Node) htmx.Node {
-	return htmx.Div(
-		htmx.ClassNames{},
-		htmx.Div(
-			htmx.ClassNames{},
-			drawers.Drawer(
-				drawers.Props{
-					ClassNames: htmx.ClassNames{
-						"lg:drawer-open": true,
-					},
+	return drawers.Drawer(
+		drawers.Props{
+			ClassNames: htmx.ClassNames{
+				"drawer-open": true,
+			},
+		},
+		drawers.DrawerToggle(
+			drawers.ToggleProps{},
+		),
+		drawers.DrawerContent(
+			drawers.DrawerContentProps{},
+			middleware.Toasts(),
+			htmx.Div(
+				htmx.ClassNames{
+					"h-full":        true,
+					"max-w-full":    true,
+					"overflow-auto": true,
+					"w-full":        true,
 				},
-				drawers.DrawerContent(
-					drawers.DrawerContentProps{},
-					middleware.Toasts(),
-					htmx.Div(
-						htmx.ClassNames{
-							"h-full":        true,
-							"max-w-full":    true,
-							"overflow-auto": true,
-							"w-full":        true,
-						},
-						htmx.Div(
-							htmx.ClassNames{
-								"flex":        true,
-								"h-full":      true,
-								"flex-col":    true,
-								"bg-base-200": true,
+				htmx.Div(
+					htmx.ClassNames{
+						"flex":        true,
+						"h-full":      true,
+						"flex-col":    true,
+						"bg-base-200": true,
+					},
+					navbars.Navbar(
+						navbars.Props{
+							ClassNames: htmx.ClassNames{
+								"navbar":      true,
+								"z-10":        true,
+								"px-3":        true,
+								"bg-base-100": true,
 							},
-							navbars.Navbar(
-								navbars.Props{
+						},
+						navbars.Start(
+							navbars.StartProps{
+								ClassNames: htmx.ClassNames{
+									"gap-3": true,
+								},
+							},
+							drawers.DrawerOpen(
+								drawers.DrawerOpenProps{
+									ID: "global-drawer",
 									ClassNames: htmx.ClassNames{
-										"navbar":      true,
-										"z-10":        true,
-										"px-3":        true,
-										"bg-base-100": true,
+										"btn-sm":      true,
+										"btn-square":  true,
+										"btn-primary": false,
 									},
 								},
-								navbars.Start(
-									navbars.StartProps{
-										ClassNames: htmx.ClassNames{
-											"gap-3": true,
-										},
-									},
-									drawers.DrawerOpen(
-										drawers.DrawerOpenProps{
-											ID: "global-drawer",
-											ClassNames: htmx.ClassNames{
-												"btn-sm":      true,
-												"btn-square":  true,
-												"btn-primary": false,
-											},
-										},
-										heroicons.Bars3Outline(
-											icons.IconProps{},
-										),
-									),
-								),
-								navbars.End(
-									navbars.EndProps{},
-									ProfileMenu(
-										ProfileMenuProps{
-											User: p.User,
-										},
-									),
+								heroicons.Bars3Outline(
+									icons.IconProps{},
 								),
 							),
-							htmx.Group(
-								children...,
+						),
+						navbars.End(
+							navbars.EndProps{},
+							ProfileMenu(
+								ProfileMenuProps{
+									User: p.User,
+								},
 							),
 						),
 					),
-				),
-				drawers.DrawerSide(
-					drawers.DrawerSideProps{},
-					drawers.DrawerSide(
-						drawers.DrawerSideProps{
-							ClassNames: htmx.ClassNames{
-								"border-r":               true,
-								"border-neutral-content": true,
-								"bg-base-100":            true,
-								"bg-base-200":            false,
-							},
-						},
-						dividers.Divider(
-							dividers.Props{},
-						),
-						MainMenu(
-							MainMenuProps{
-								Path: p.Path,
-							},
-						),
-						dividers.Divider(
-							dividers.Props{},
-						),
-						UserMenu(
-							UserMenuProps{
-								Path: p.Path,
-							},
-						),
+					htmx.Group(
+						children...,
 					),
 				),
+			),
+		),
+		drawers.DrawerSide(
+			drawers.DrawerSideProps{
+				ClassNames: htmx.ClassNames{
+					"border-r":               true,
+					"border-neutral-content": true,
+					"bg-base-100":            true,
+					"bg-base-200":            false,
+				},
+			},
+			MainMenu(
+				MainMenuProps{
+					Path: p.Path,
+				},
+			),
+			dividers.Divider(
+				dividers.Props{},
+			),
+			UserMenu(
+				UserMenuProps{
+					Path: p.Path,
+				},
 			),
 		),
 	)
